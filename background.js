fc564caaba4decde0dd74dc116bf3d5247982150
jdh <0815>
2025-05-23 21:15:43 +0900

    첫 커밋

---
+++ background.js
@@ -0,0 +1,4 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("뉴스 신뢰도 사이드바 확장 프로그램이 설치되었습니다.");
+});
---
+++ popup.js
@@ -0,0 +1,5 @@
+
+document.getElementById("analyzeBtn").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.tabs.sendMessage(tab.id, { action: "analyze" });
+});
